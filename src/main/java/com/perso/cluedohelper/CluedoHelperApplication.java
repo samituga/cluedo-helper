a62469460eebe11dbdae0aff90ce5cee3bdf86a7
samuel.goncalves <samuel.c.goncalves07@gmail.com>
2021-10-18 20:13:40 +0100

    init

---
+++ src/main/java/com/perso/cluedohelper/CluedoHelperApplication.java
@@ -0,0 +1,13 @@
+package com.perso.cluedohelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluedoHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CluedoHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/perso/cluedohelper/CluedoHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perso.cluedohelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CluedoHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
