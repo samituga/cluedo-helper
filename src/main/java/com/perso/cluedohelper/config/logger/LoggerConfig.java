5d7b56c9047368a9e459140e4803cb4c89717547
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2021-10-29 23:04:37 +0100

    feature/base-architecture | Added some javadoc

--- src/main/java/com/perso/cluedohelper/config/logger/LoggerConfig.java
+++ src/main/java/com/perso/cluedohelper/config/logger/LoggerConfig.java
@@ -11,9 +11,17 @@ import java.lang.reflect.Field;
 
 import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
+/**
+ * Configuration class to facilitate the injection of a {@link Logger} bean
+ */
 public class LoggerConfig {
 
-
+	/**
+	 * {@link Logger} bean to be injected
+	 *
+	 * @param injectionPoint the point in code where the {@link Logger} is being injected
+	 * @return A {@link Logger} for the respective class
+	 */
 	@Bean
 	@Scope(SCOPE_PROTOTYPE)
 	Logger logger(InjectionPoint injectionPoint) {
