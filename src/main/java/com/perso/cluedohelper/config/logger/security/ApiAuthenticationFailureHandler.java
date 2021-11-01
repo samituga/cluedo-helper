f66807bbbbd0d507cb20e300037444a98f096dc1
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2021-11-01 13:04:23 +0000

    Added support for spring boot security

---
+++ src/main/java/com/perso/cluedohelper/config/logger/security/ApiAuthenticationFailureHandler.java
@@ -0,0 +1,49 @@
+package com.perso.cluedohelper.config.logger.security;
+
+import com.perso.cluedohelper.exception.response.ErrorResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import static com.perso.cluedohelper.util.ApiConstants.CORRELATION_ID_KEY;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Handles API key authentication related failures
+ */
+@Component
+@RequiredArgsConstructor
+public class ApiAuthenticationFailureHandler implements AuthenticationEntryPoint {
+
+	private final MappingJackson2HttpMessageConverter messageConverter;
+
+	@Override
+	public void commence(final HttpServletRequest request,
+						 final HttpServletResponse response,
+						 final AuthenticationException exception) throws IOException {
+
+		ErrorResponse errorResponse = ErrorResponse.builder() // TODO: 01/11/2021 Get the information for this fields
+			.internalCode("internalCode")
+			.message("message")
+			.correlationId(ThreadContext.get(CORRELATION_ID_KEY))
+			.timestamp(LocalDateTime.now())
+			.build();
+
+		final ServerHttpResponse outputMessage = new ServletServerHttpResponse(response);
+		outputMessage.setStatusCode(requireNonNull(HttpStatus.FORBIDDEN));
+
+		messageConverter.write(errorResponse, MediaType.APPLICATION_JSON, outputMessage);
+	}
+}
---
+++ src/main/java/com/perso/cluedohelper/config/logger/security/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.perso.cluedohelper.config.logger.security;
+
+import com.perso.cluedohelper.filter.APIKeyAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * Configuration class for the security of the API
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Value("${cluedohelper.http.auth-token-header-name}")
+	private String principalRequestHeader;
+
+	@Value("${cluedohelper.http.auth-token}")
+	private String principalRequestValue;
+
+	private final ApiAuthenticationFailureHandler apiAuthenticationFailureHandler;
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		final APIKeyAuthFilter apiKeyAuthFilter = apiKeyAuthFilter();
+		httpSecurity
+			.cors().and()
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.addFilter(apiKeyAuthFilter).authorizeRequests().antMatchers("/**").authenticated()
+			.and()
+			.exceptionHandling()
+			.authenticationEntryPoint(apiAuthenticationFailureHandler);
+	}
+
+	@Override
+	public void configure(final WebSecurity web) {
+		web.ignoring()
+			.antMatchers(HttpMethod.OPTIONS, "/**");
+	}
+
+
+	/**
+	 * Builds the APIKeyAuthFilter
+	 *
+	 * @return The APIKeyAuthFilter
+	 */
+	private APIKeyAuthFilter apiKeyAuthFilter() {
+		APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
+		filter.setAuthenticationManager(authentication -> {
+			String principal = (String) authentication.getPrincipal();
+			if (!principalRequestValue.equals(principal)) {
+				throw new BadCredentialsException("The API key was not found or not the expected value.");
+			}
+			authentication.setAuthenticated(true);
+			return authentication;
+		});
+		return filter;
+	}
+}
