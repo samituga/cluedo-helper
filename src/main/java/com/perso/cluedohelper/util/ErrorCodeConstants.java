4660c759380911977ee557ea48b2b7be26b48a16
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2021-11-02 19:51:51 +0000

    Added missing annotation and some javadoc

--- src/main/java/com/perso/cluedohelper/util/ErrorCodeConstants.java
+++ src/main/java/com/perso/cluedohelper/util/ErrorCodeConstants.java
@@ -2,11 +2,12 @@ package com.perso.cluedohelper.util;
 
 import lombok.experimental.UtilityClass;
 
+/**
+ * Provides the error codes, must be replicated with the errors.yml resources file
+ */
 @UtilityClass
 public class ErrorCodeConstants {
 
 	public static final String CH000 = "CH000";
 	public static final String CH001 = "CH001";
-
-
 }
