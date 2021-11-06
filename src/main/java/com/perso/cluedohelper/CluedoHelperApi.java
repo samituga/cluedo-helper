4047383c595ddbd405ca9c03c808c973f7649978
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2021-11-06 11:40:38 +0000

    Updted library dependency version and added sonatype snapshot repository

--- src/main/java/com/perso/cluedohelper/CluedoHelperApi.java
+++ src/main/java/com/perso/cluedohelper/CluedoHelperApi.java
@@ -1,12 +1,15 @@
 package com.perso.cluedohelper;
 
+import com.perso.cluedohelper.config.LibraryConfigurationReference;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * Cluedo helper API.
  */
 @SpringBootApplication
+@Import(LibraryConfigurationReference.class)
 public class CluedoHelperApi {
 
   public static void main(String[] args) {
---
+++ src/main/java/com/perso/cluedohelper/config/LibraryConfigurationReference.java
@@ -0,0 +1,8 @@
+package com.perso.cluedohelper.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("io.github.samituga.cluedohelperlibrary.config")
+public class LibraryConfigurationReference {}
